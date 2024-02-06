4faa4479eda6c31807b3b30be84cebcbcc243f08
TIM\User <timonchik2001@mail.ru>
2024-02-06 17:54:29 +0300

    feat: solved 4 task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -81,8 +81,11 @@ function findElement(arr, value) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  return arr.reduce(
+    (count, current) => (current === item ? count + 1 : count),
+    0
+  );
 }
 
 /**
