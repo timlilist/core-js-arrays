724491096efe311afa7625b8a113710b39ec8023
TIM\User <timonchik2001@mail.ru>
2024-02-07 14:04:30 +0300

    feat: solved 7 task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -132,8 +132,10 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) return 0;
+  const summa = arr.reduce((a, b) => a + b, 0);
+  return +(summa / arr.length).toFixed(2);
 }
 
 /**
