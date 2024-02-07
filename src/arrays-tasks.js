7a5e08ea331f8a495ce9602e0007ea851191726a
TIM\User <timonchik2001@mail.ru>
2024-02-07 14:08:04 +0300

    feat: solveh 8 task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -148,8 +148,8 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  return arr.every((item) => item.length === arr[0].length);
 }
 
 /**
