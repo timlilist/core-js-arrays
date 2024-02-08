e9d1e8e2e56e0159cfb84ad8b1c894dde1b2e6cd
TIM\User <timonchik2001@mail.ru>
2024-02-08 20:47:23 +0300

    feat: solved 23 task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -401,8 +401,8 @@ function getElementByIndices(arr, indices) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.reduce((count, item) => (item ? count : count + 1), 0);
 }
 
 /**
